8a26425c4b7b9a796c2814b7af6c866647d4dda0
joymet33 <joymet33@gmail.com>
2024-06-03 12:18:02 +0900

    (add) Contents.jsx

---
+++ src/pages/Contents.jsx
@@ -0,0 +1,16 @@
+const Contents = () => {
+  return (
+    <div>
+      <h1>제목</h1>
+      <p>블라블라 왈라왈라</p>
+      <button>저장</button>
+      <button>취소</button>
+    </div>
+  );
+};
+
+export default Contents;
+
+const ContentTitle = styled.div`
+  font-size: 35pt;
+`;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SupabaseExample from '../pages/SupabaseExample.jsx';
 import Layout from '../components/layout/Layout.jsx';
+import Contents from '../pages/Contents.jsx';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="/:userId/blog" element={<Contents />}></Route>
           <Route path="/supabase" element={<SupabaseExample />} />
           {/*   레이아웃이 필요한 페이지는 supabase와 같이 이 안에 Route 를 넣어주세요 로그인 관련 Route는 밖에 빼주세요.*/}
         </Route>
