88036e0283b043dd434a2d9cbc6585a22a1583c8
백현명 <whitewise95@users.noreply.github.com>
2024-06-03 16:31:40 +0900

    fix: slice 명 변경

---
+++ src/redux/slices/blogSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  posts: [],
+};
+
+
+const blogSlice = createSlice({
+  name: 'blog', // 이 모듈의 이름
+  initialState,   // 이 모듈의 초기상태 값
+  reducers: {  // 이 모듈의 Reducer 로직
+
+  },
+});
+
+// 액션크리에이터는 컴포넌트에서 사용하기 위해 export 하고
+// export const { } = counterSlice.actions;
+// reducer 는 configStore에 등록하기 위해 export default 합니다.
+export default blogSlice.reducer;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SupabaseExample from '../pages/SupabaseExample.jsx';
 import Layout from '../components/layout/Layout.jsx';
+import PostsList from '../pages/PostsList.jsx';
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/supabase" element={<SupabaseExample />} />
+          <Route path="/blog/:userId" element={<PostsList />} />
           {/*   레이아웃이 필요한 페이지는 supabase와 같이 이 안에 Route 를 넣어주세요 로그인 관련 Route는 밖에 빼주세요.*/}
         </Route>
       </Routes>
