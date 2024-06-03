2da1748a12a6b4607434e1cc74e0d5fc9b9a3c7a
LinQuixote <leein9404@naver.com>
2024-06-04 06:17:48 +0900

    feat : 회원가입 유효성 검사 구현

--- src/pages/Join/JoinPage.jsx
+++ src/pages/Join/JoinPage.jsx
@@ -1,43 +1,77 @@
-import { useState } from 'react';
+import { produce } from 'immer';
+import { useReducer } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import supabase from '../../config/supabase';
 import { useUser } from '../../contexts/login.context';
+import { userResolver } from '../../util/userSchema';
 
-let CREATE_USER_MESSAGE;
+const VALIDATION = 'form-validation';
+const CREATE_FAIL = 'create-fail';
+const CREATE_SUCCESS = 'create-success';
+
+function submitResultReducer(state, action) {
+  switch (action.type) {
+    case CREATE_FAIL:
+      return produce(state, (draft) => {
+        draft.message = '중복되는 이메일입니다.';
+      });
+
+    case CREATE_SUCCESS:
+      return produce(state, (draft) => {
+        draft.message = '이메일 인증으로 로그인을 완료해주세요.';
+        draft.email = action.payolad.email;
+      });
+
+    case VALIDATION:
+      return produce(state, (draft) => {
+        draft.vaildMessage = action.payolad;
+      });
+  }
+}
 
 function JoinPage() {
+  const [submitStatus, dispatch] = useReducer(submitResultReducer, {
+    message: null,
+    email: null,
+    vaildMessage: {},
+  });
+  console.log('submitStatus :', submitStatus);
   const { userData } = useUser();
-  const [confirmEmail, setConfirmEmail] = useState(null);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const formDataObj = Object.fromEntries(formData.entries());
+
+    const errors = userResolver(formDataObj);
+    console.log('errors :', errors);
+    if (Object.keys(errors).length !== 0) {
+      dispatch({
+        type: VALIDATION,
+        payolad: errors,
+      });
+      return;
+    }
+
     const { data, error } = await supabase.auth.signUp({
       email: formDataObj.email,
       password: formDataObj.password,
     });
-    if (data) {
+    console.log(data, error);
+    if (data.user) {
       // 로그인 입력 이메일을 요청해야함.
-      setConfirmEmail(data.user.email);
-    }
-    if (error) {
-      setConfirmEmail(false);
+      dispatch({
+        type: CREATE_SUCCESS,
+        payolad: data.user.email,
+      });
+    } else {
+      dispatch({
+        type: CREATE_FAIL,
+      });
     }
   };
-  switch (confirmEmail) {
-    case null:
-      // 초기값
-      CREATE_USER_MESSAGE = '';
-      break;
-    case false:
-      // 인증실패
-      CREATE_USER_MESSAGE = '잘못된 입력입니다.';
-      break;
-    default:
-      CREATE_USER_MESSAGE = '이메일 인증으로 로그인을 완료해주세요.';
-  }
 
   if (userData.isLogedIn) {
     return navigate('/');
@@ -45,18 +79,26 @@ function JoinPage() {
 
   return (
     <div>
-      <div>{!confirmEmail && CREATE_USER_MESSAGE}</div>
-      {!confirmEmail ? (
-        <form onSubmit={handleSubmit}>
-          <input name="email" type="email" />
-          <input name="password" type="password" />
-          <input name="password2" type="password" />
-          <button type="submit">가입</button>
-        </form>
+      {!submitStatus.email ? (
+        <>
+          <h3>{Object.keys(submitStatus.vaildMessage).length !== 0 && submitStatus.message}</h3>
+          <form onSubmit={handleSubmit}>
+            <input name="email" type="email" />
+            {submitStatus.vaildMessage['email'] &&
+              submitStatus.vaildMessage['email'].map((msg) => <span key={msg}>{msg}</span>)}
+            <input name="password" type="password" />
+            {submitStatus.vaildMessage['password'] &&
+              submitStatus.vaildMessage['password'].map((msg) => <span key={msg}>{msg}</span>)}
+            <input name="password2" type="password" />
+            {submitStatus.vaildMessage['password2'] &&
+              submitStatus.vaildMessage['password2'].map((msg) => <span key={msg}>{msg}</span>)}
+            <button type="submit">가입</button>
+          </form>
+        </>
       ) : (
         <div>
-          <h3>{CREATE_USER_MESSAGE}</h3>
-          <h4>{confirmEmail}</h4>
+          <h3>{submitStatus.message}</h3>
+          <h4>{submitStatus.email}</h4>
         </div>
       )}
     </div>
